5b19098d6c2aecb200fb0aea50dfb2fff7a451da
unknown <apichart.a@BKKMITNTB6771.ad.insidemedia.net>
2015-04-21 18:42:58 +0700

    no message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Node.js is running1...!!");
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200,{"Content-Type" : "text/html"});
-  res.write('<span style="color:red;">APICHART</span>');
+  res.write('<span style="color:red;">APICHART APINYANSUDJA</span>\n');
   res.end('Hello \n World\n');
 }).listen(9999);
 
